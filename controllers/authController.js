3c7441d336e81abee04124b663882ae26078868c
amine <zouariamine52@gmail.com>
2022-11-19 13:34:46 +0100

    prepare-for-deployment

--- controllers/authController.js
+++ controllers/authController.js
@@ -102,7 +102,6 @@ exports.protect = catchAsync(async (req, res, next) => {
 
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
-    console.log(req.user.role);
     // roles ['admin', 'lead-guide']. role='user'
     if (!roles.includes(req.user.role)) {
       return next(
